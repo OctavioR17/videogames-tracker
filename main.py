df4dff7c14a9aca95339e585d141d72ff32296e3
OctavioR17 <octaviorogo17@gmail.com>
2026-05-25 16:04:04 -0600

    Implementacion de estructura de control - Diccionario

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 
+# Cargar juegos desde games.json
 try:
 	with open("games.json", "r") as file:
 		games = json.load(file)
@@ -15,16 +16,35 @@ while True:
 	option = input("Selecciona una opción: ")
 
 	if option == "1":
-		game = input("Nombre del juego: ")
+		#Pedir datos
+		name = input("Nombre del juego: ")
+		plataforma = input("Plataforma del juego: ")
+		estado = input("Estado del juego: ")
+
+        # Crear diccionario
+		game = {
+			"nombre": name,
+			"plataforma": plataforma,
+			"estado": estado
+		}
+
+		#Agregar a la lista
 		games.append(game)
+
+		#Guardar en el json
 		with open("games.json", "w") as file:
 			json.dump(games, file, indent=4)
-		print("Juego agregado")
-	
+
+		print("juego agregado")
+
 	elif option == "2":
 		print("\nTus juegos:")
 		for game in games:
-			print("-", game)
+			print(f"""
+		 Nombre: {game["nombre"]}
+		 Plataforma: {game["plataforma"]}
+		 Estado: {game["estado"]}
+			""")
 
 	elif option == "3":
 		print("Hasta luego")
